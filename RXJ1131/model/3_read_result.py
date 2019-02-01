6fcd1cd7da3665f7186f4804e9c4fb5eb53cee76
dartoon <dingxuheng@mail.bnu.edu.cn>
2019-02-01 10:16:23 -0800

    Update 3_read_result.py

--- RXJ1131/model/3_read_result.py
+++ RXJ1131/model/3_read_result.py
@@ -11,16 +11,110 @@ import numpy as np
 import astropy.io.fits as pyfits
 import matplotlib.pyplot as plt
 import pickle
-#PSFno = 
-#subg= 2
-
-
+import corner
+from lenstronomy.SimulationAPI.simulations import Simulation
+from lenstronomy.Plots.output_plots import LensModelPlot
+import copy
+import sys
+sys.path.insert(0,'../../py_tools')
+from mask_objects import mask_obj
 #The values to pick up:
-filename = "fit_PSFi_QSO_mask/result_PSF3_Dhost_Dlens_subg2"
+filename = "fit_PSFi_QSO_mask/result_PSF3_Dhost_Dlens_subg3"
+psfno = 3
+subg = 2
 
 result = pickle.load(open(filename,'rb'))
-
 if len(result) == 2:
     fit_result, trans_result = result
 elif len(result) ==3:
     fit_result, trans_result, kwargs_psf_updated = result
+'''
+
+lens_result, source_result, lens_light_result, ps_result, cosmo_result,\
+chain_list, param_list, samples_mcmc, param_mcmc, dist_mcmc = fit_result
+mcmc_new_list, labels_new = trans_result
+
+ct = 20
+lens_image = pyfits.getdata('RXJ1131_cutout.fits')   #!!!need to change
+lens_rms = pyfits.getdata('RXJ1131_stdd.fits')    #!!!need to change
+lens_image = lens_image[ct:-ct,ct:-ct]
+lens_rms = lens_rms[ct:-ct,ct:-ct]
+sigma_bkg = 0.007   # inference from 0_cutout  #!!!need to change
+exp_time = 1980  # exposure time (arbitrary units, flux per pixel is in units #photons/exp_time unit)
+numPix = len(lens_image)  # cutout pixel size
+deltaPix = 0.05  # pixel size in arcsec (area per pixel = deltaPix**2)  #!!!need to change
+fwhm = 0.16  # full width half max of PSF
+SimAPI = Simulation()
+kwargs_data = SimAPI.data_configure(numPix, deltaPix, exp_time, sigma_bkg)
+
+psfname_list = ['../PSF{0}.fits'.format(i) for i in range(4)]
+psfs = []
+for i in range(len(psfname_list)):
+    psf_i = pyfits.getdata(psfname_list[i])
+    _, psf_mask = mask_obj(img=psf_i,snr=2.0,exp_sz=1.7,pltshow=0)
+    if 1 in psf_mask[0]:
+        if len(psf_mask) > 1:
+            psf_mask = np.sum(np.asarray(psf_mask),axis=0)
+        elif len(psf_mask) == 1:
+            psf_mask = psf_mask[0]
+        psf_mask = (1 - (psf_mask != 0)*1.)
+        psf_clear = copy.deepcopy(psf_i)
+        for i in range(len(psf_i)):
+            for j in range(len(psf_i)):
+                if psf_mask[i, j] == 0:
+                    psf_clear[i, j] = (psf_i[-i-1, -j-1]*psf_mask[-i-1, -j-1])
+        psf_i = psf_clear
+    psfs.append(psf_i)
+psf = psfs[psfno]
+psf = psf/psf.sum()
+kwargs_psf = SimAPI.psf_configure(psf_type='PIXEL', fwhm=fwhm, kernelsize=len(psf), deltaPix=deltaPix,kernel=psf)
+if len(result) ==3:
+   kwargs_psf = kwargs_psf_updated
+
+kwargs_numerics = {'subgrid_res': subg, 'psf_subgrid': False}
+
+lens_model_list = ['SPEMD', 'SIS', 'SHEAR']
+lens_light_model_list = ['SERSIC_ELLIPSE','SERSIC']
+source_model_list = ['SERSIC_ELLIPSE','SERSIC_ELLIPSE']
+point_source_list = ['LENSED_POSITION']
+kwargs_model = {'lens_model_list': lens_model_list,
+                               'source_light_model_list': source_model_list,
+                               'lens_light_model_list': lens_light_model_list,
+                               'point_source_model_list': point_source_list,
+                               'fixed_magnification_list': [False]
+                             }
+
+lensPlot = LensModelPlot(kwargs_data, kwargs_psf, kwargs_numerics, kwargs_model, lens_result, source_result,
+                             lens_light_result, ps_result, arrow_size=0.02, cmap_string="gist_heat")
+    
+f, axes = plt.subplots(2, 3, figsize=(16, 8), sharex=False, sharey=False)
+
+lensPlot.data_plot(ax=axes[0,0])
+lensPlot.model_plot(ax=axes[0,1])
+lensPlot.normalized_residual_plot(ax=axes[0,2], v_min=-6, v_max=6)
+lensPlot.source_plot(ax=axes[1, 0],convolution=False, deltaPix_source=0.01, numPix=100)
+lensPlot.convergence_plot(ax=axes[1, 1], v_max=1)
+lensPlot.magnification_plot(ax=axes[1, 2])
+f.tight_layout()
+f.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0., hspace=0.05)
+plt.show()
+
+f, axes = plt.subplots(2, 3, figsize=(16, 8), sharex=False, sharey=False)
+lensPlot.decomposition_plot(ax=axes[0,0], text='Lens light', lens_light_add=True, unconvolved=True)
+lensPlot.decomposition_plot(ax=axes[1,0], text='Lens light convolved', lens_light_add=True)
+lensPlot.decomposition_plot(ax=axes[0,1], text='Source light', source_add=True, unconvolved=True)
+lensPlot.decomposition_plot(ax=axes[1,1], text='Source light convolved', source_add=True)
+lensPlot.decomposition_plot(ax=axes[0,2], text='All components', source_add=True, lens_light_add=True, unconvolved=True)
+lensPlot.decomposition_plot(ax=axes[1,2], text='All components convolved', source_add=True, lens_light_add=True, point_source_add=True)
+f.tight_layout()
+f.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0., hspace=0.05)
+plt.show()
+print(lens_result, source_result, lens_light_result, ps_result)
+print("number of non-linear parameters in the MCMC process: ", len(param_mcmc))
+print("parameters in order: ", param_mcmc)
+print("number of evaluations in the MCMC process: ", np.shape(samples_mcmc)[0])
+    
+mcmc_new_array = np.array(mcmc_new_list)
+fig = corner.corner(mcmc_new_array[mcmc_new_array[:,2]!=0], labels=labels_new, show_titles=True)
+plt.show()
+'''
