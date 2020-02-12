4b8fc32c312190a18702e6db04616076d7dc90f6
dartoon <dingxuheng@mail.bnu.edu.cn>
2020-02-11 21:28:32 -0800

    SDSS1206 using singel host

--- share_tools/summary.py
+++ share_tools/summary.py
@@ -16,74 +16,77 @@ import copy
 from adjustText import adjust_text   # avoid the overlapping while ploting
 from read_chisq import return_chisq 
 
-IDs = ['0_HE0435', '1_RXJ1131', '2_WFI2033', '3_SDSS1206', '4_HE1104', '5_SDSS0246', '6_HS2209', '7_HE0047']
-#for i in range(len(IDs)):
-#    ID = IDs[i][2:]
-#    root = '../'+ IDs[i]+ '/model/'
-#    pkl_folderfiles = glob.glob(root+"2nd_fit_PSFi_PSFrecons/result_PSF*.pkl")
-#    pkl_files = [pkl_folderfiles[i].split('/')[-1] for i in range(len(pkl_folderfiles))]
-#    
-#    psf_num = np.max([int(pkl_files[i].split('_PSF')[1]) for i in range(len(pkl_files))]) + 1
-#    
-#    if float(len(pkl_files)/6) != len(pkl_files)/6. :
-#        raise ValueError("The number of the pickle result is the Multiples of six")
-#    
-#    PSFno_ = range(psf_num)
-#    #The values to pick up:
-#    filename = pkl_folderfiles[0]
-#    result = pickle.load(open(filename,'rb'))
-#    fit_result, trans_result, kwargs_material, model_lists  = result
-#    pick_names = trans_result[-1]
-#    _, _, _, _, lens_mask = kwargs_material
-#    stdd_file = glob.glob(root+'fit_PSFi_QSOmask/*_stdd_boost.fits')
-#    if len(stdd_file)>1:
-#        raise ValueError("More than one *_stdd_boost.fits exits") 
-#    rms_boost = pyfits.getdata(stdd_file[0])
-#    lens_mask[rms_boost == rms_boost.max()] = 0
-#    plt.imshow(lens_mask,origin='low')
-#    plt.show()
-#    
-#    fair_PSF = True
-#    labels = []
-#    fit_value_l_list, fit_value_m_list, fit_value_h_list, = [], [], []
-#    chisq = []
-#    fixgamma_list = []
-#    
-#    folder_i = ['2nd_fit_PSFi_QSOmask', '2nd_fit_PSFi_PSFrecons']
-#    for folder in folder_i:
-#        filenames = glob.glob(root+'{0}/result_PSF?_*'.format(folder))        
-#        filenames.sort()
-#        for filename in filenames:
-#            PSFtyp = filename.split('/')[-1].split('_')[1]
-#            if int(PSFtyp.split('PSF')[1]) in PSFno_:
-#                subg = filename[(filename).find('subg')+len('subg')]
-#                fit_type = filename.split('/')[-2].split('_')[-1]
-#                fixgamma = (filename.split('gammafix')[1]).split('_subg')[0]
-#                if fixgamma not in fixgamma_list:
-#                    fixgamma_list.append(fixgamma)
-#                print filename
-#                result = pickle.load(open(filename,'rb'))
-#                fit_result, trans_result, kwargs_material, model_lists  = result
-#                fit_value_l_i, fit_value_m_i, fit_value_h_i, = [], [], []
-#                for pick in range(len(pick_names)):
-#                    fit_value = np.asarray(trans_result[0])[:,pick]
-#                    fit_value_l_i.append(np.percentile(fit_value,16,axis=0))
-#                    fit_value_m_i.append(np.percentile(fit_value,50,axis=0))
-#                    fit_value_h_i.append(np.percentile(fit_value,84,axis=0))
-#                fit_value_l_list.append(fit_value_l_i)
-#                fit_value_m_list.append(fit_value_m_i)
-#                fit_value_h_list.append(fit_value_h_i)
-#                labels.append("{0}, fixgamma_{1}, subg_{2}, {3}".format(PSFtyp, fixgamma, subg, fit_type))
-#                chisq.append(repr(round(return_chisq(filename, lens_mask= lens_mask, fair_mask=True, fair_PSF = fair_PSF),3)))   
-#    labels_orig = copy.deepcopy(labels)
-#    fit_value_l_list = [x for _,x in sorted(zip(labels,fit_value_l_list))]  #!!!
-#    fit_value_m_list = [x for _,x in sorted(zip(labels,fit_value_m_list))]
-#    fit_value_h_list = [x for _,x in sorted(zip(labels,fit_value_h_list))]
-#    chisq = [x for _,x in sorted(zip(labels,chisq))]
-#    labels = [x for _,x in sorted(zip(labels,labels))]  # Sort the label at the last
-#    picklename = ID+'_2nd_run_summary.pkl'
-#    to_be_saved = [[fit_value_l_list, fit_value_m_list, fit_value_h_list],chisq, labels, pick_names]
-#    pickle.dump(to_be_saved, open(picklename, 'wb'))
+# IDs = ['0_HE0435', '1_RXJ1131', '2_WFI2033', '3_SDSS1206', '4_HE1104', '5_SDSS0246', '6_HS2209', '7_HE0047']
+# folder = '2nd_fit_'
+IDs = ['3_SDSS1206']
+folder_type = 'singel_fit_'
+
+# for i in range(len(IDs)):
+#     ID = IDs[i][2:]
+#     root = '../'+ IDs[i]+ '/model/'
+#     pkl_folderfiles = glob.glob(root+folder_type+"PSFi_PSFrecons/result_PSF*.pkl")
+#     pkl_files = [pkl_folderfiles[i].split('/')[-1] for i in range(len(pkl_folderfiles))]
+#     psf_num = np.max([int(pkl_files[i].split('_PSF')[1]) for i in range(len(pkl_files))]) + 1
+    
+#     if float(len(pkl_files)/6) != len(pkl_files)/6. :
+#         raise ValueError("The number of the pickle result is the Multiples of six")
+    
+#     PSFno_ = range(psf_num)
+#     #The values to pick up:
+#     filename = pkl_folderfiles[0]
+#     result = pickle.load(open(filename,'rb'))
+#     fit_result, trans_result, kwargs_material, model_lists  = result
+#     pick_names = trans_result[-1]
+#     _, _, _, _, lens_mask = kwargs_material
+#     stdd_file = glob.glob(root+'fit_PSFi_QSOmask/*_stdd_boost.fits')
+#     if len(stdd_file)>1:
+#         raise ValueError("More than one *_stdd_boost.fits exits") 
+#     rms_boost = pyfits.getdata(stdd_file[0])
+#     lens_mask[rms_boost == rms_boost.max()] = 0
+#     plt.imshow(lens_mask,origin='low')
+#     plt.show()
+    
+#     fair_PSF = True
+#     labels = []
+#     fit_value_l_list, fit_value_m_list, fit_value_h_list, = [], [], []
+#     chisq = []
+#     fixgamma_list = []
+    
+#     folder_i = [folder_type+'PSFi_QSOmask', folder_type+'PSFi_PSFrecons']
+#     for folder in folder_i:
+#         filenames = glob.glob(root+'{0}/result_PSF?_*'.format(folder))        
+#         filenames.sort()
+#         for filename in filenames:
+#             PSFtyp = filename.split('/')[-1].split('_')[1]
+#             if int(PSFtyp.split('PSF')[1]) in PSFno_:
+#                 subg = filename[(filename).find('subg')+len('subg')]
+#                 fit_type = filename.split('/')[-2].split('_')[-1]
+#                 fixgamma = (filename.split('gammafix')[1]).split('_subg')[0]
+#                 if fixgamma not in fixgamma_list:
+#                     fixgamma_list.append(fixgamma)
+#                 print filename
+#                 result = pickle.load(open(filename,'rb'))
+#                 fit_result, trans_result, kwargs_material, model_lists  = result
+#                 fit_value_l_i, fit_value_m_i, fit_value_h_i, = [], [], []
+#                 for pick in range(len(pick_names)):
+#                     fit_value = np.asarray(trans_result[0])[:,pick]
+#                     fit_value_l_i.append(np.percentile(fit_value,16,axis=0))
+#                     fit_value_m_i.append(np.percentile(fit_value,50,axis=0))
+#                     fit_value_h_i.append(np.percentile(fit_value,84,axis=0))
+#                 fit_value_l_list.append(fit_value_l_i)
+#                 fit_value_m_list.append(fit_value_m_i)
+#                 fit_value_h_list.append(fit_value_h_i)
+#                 labels.append("{0}, fixgamma_{1}, subg_{2}, {3}".format(PSFtyp, fixgamma, subg, fit_type))
+#                 chisq.append(repr(round(return_chisq(filename, lens_mask= lens_mask, fair_mask=True, fair_PSF = fair_PSF),3)))   
+#     labels_orig = copy.deepcopy(labels)
+#     fit_value_l_list = [x for _,x in sorted(zip(labels,fit_value_l_list))]  #!!!
+#     fit_value_m_list = [x for _,x in sorted(zip(labels,fit_value_m_list))]
+#     fit_value_h_list = [x for _,x in sorted(zip(labels,fit_value_h_list))]
+#     chisq = [x for _,x in sorted(zip(labels,chisq))]
+#     labels = [x for _,x in sorted(zip(labels,labels))]  # Sort the label at the last
+#     picklename = ID+'singel_host_run_summary.pkl'
+#     to_be_saved = [[fit_value_l_list, fit_value_m_list, fit_value_h_list],chisq, labels, pick_names]
+#     pickle.dump(to_be_saved, open(picklename, 'wb'))
 
 
 #%%
@@ -91,7 +94,8 @@ for i in range(len(IDs)):
     print i, ':', IDs[i]
 inp = input("Which source:\n")
 ID = IDs[inp][2:]
-filename = ID+'_2nd_run_summary.pkl'
+# filename = ID+'2nd_run_summary.pkl'
+filename = ID+'_singel_host_run_summary.pkl'
 result = pickle.load(open(filename,'rb'))
 fit_values, chisq, labels, pick_names = result
 fit_value_l_list, fit_value_m_list, fit_value_h_list = fit_values
--- share_tools/weighting_inference.py
+++ share_tools/weighting_inference.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import pickle
 import glob
 import copy
-from adjustText import adjust_text   # avoid the overlapping while ploting
 IDs = ['0_HE0435', '1_RXJ1131', '2_WFI2033', '3_SDSS1206', '4_HE1104', '5_SDSS0246', '6_HS2209', '7_HE0047']
 
 weighted_value_list, rms_value_list = [], []
@@ -14,55 +13,57 @@ prop = input('flux input 0 ; Reff input 1 ; host flux image_plane 2:\n')
 #%%
 for i in range(len(IDs)):
 	ID = IDs[i][2:]
-	# print i, ID
-	filename = ID+'_2nd_run_summary.pkl'
-	result = pickle.load(open(filename,'rb'))
-	fit_values, chisq, labels, pick_names = result
-	sort_label_type=['subg_2, PSFrecons', ', subg_2, QSOmask', 'subg_3, PSFrecons',', subg_3, QSOmask']
-	fit_value_l_list, fit_value_m_list, fit_value_h_list = fit_values
-	for i in range(len(labels)):
-	    if labels[i][-17:] != sort_label_type[i%4]:
-	        raise ValueError("The labels is wrong for some reason")
-	print 
-	if prop == 0 :
+	if ID == "SDSS1206" :
+		filename = ID+'_singel_host_run_summary.pkl'
+	else:
+		filename = ID+'_2nd_run_summary.pkl'
+	# filename = ID+'_2nd_run_summary.pkl'	
+ 	result = pickle.load(open(filename,'rb'))
+ 	fit_values, chisq, labels, pick_names = result
+ 	sort_label_type=['subg_2, PSFrecons', ', subg_2, QSOmask', 'subg_3, PSFrecons',', subg_3, QSOmask']
+ 	fit_value_l_list, fit_value_m_list, fit_value_h_list = fit_values
+ 	for i in range(len(labels)):
+ 	    if labels[i][-17:] != sort_label_type[i%4]:
+ 	        raise ValueError("The labels is wrong for some reason")
+ 	if prop == 0 :
 		if 'Disk' in pick_names[2]:
-			pick = 4
+ 			pick = 4
 		else:
-			pick = 3
-	elif prop == 1:
+ 			pick = 3
+ 	elif prop == 1:
 		if 'Disk' in pick_names[2]:
-			pick = 6
+ 			pick = 6
 		else:
-			pick = 5
-	elif prop == 2:
-			pick = 1  #Flux in the image plane
-	print ID, pick_names
-	fit_value_l = [fit_value_l_list[i][pick] for i in range(len(labels))]
-	fit_value_m = [fit_value_m_list[i][pick] for i in range(len(labels))]
-	fit_value_h = [fit_value_h_list[i][pick] for i in range(len(labels))]
-	count_n = 8
-	chisq = [float(chisq[i]) for i in range(len(chisq))]
-	sort_chisq = np.argsort(np.asarray(chisq))    
-	Chisq_best = chisq[sort_chisq[0]]
-	#Chisq_last= chisq[sort_chisq[-1]]
-	Chisq_last= chisq[sort_chisq[count_n-1]]
-	weight = np.zeros(len(chisq))
-	inf_alp = (Chisq_last-Chisq_best) / (2*2.* Chisq_best)
-	for i in sort_chisq[:count_n]:
-	#for i in range(len(sort_chisq)):
-	#    weight[i] = np.exp(-1/2. * (chisq[i]-Chisq_best)/(Chisq_best))
-	    weight[i] = np.exp(-1/2. * (chisq[i]-Chisq_best)/(Chisq_best* inf_alp))
-	weighted_value = np.sum(np.array(fit_value_m)*weight) / np.sum(weight)
-	rms_value = np.sqrt(np.sum((np.array(fit_value_m)-weighted_value)**2*weight) / np.sum(weight))
-	weighted_value_list.append(weighted_value)
-	rms_value_list.append(rms_value)
-	# print pick_names[pick]
-	# print "weighted_value, rms_value:", round(weighted_value,2), round(rms_value,2)
+ 			pick = 5
+ 	elif prop == 2:
+ 			pick = 1  #Flux in the image plane
+ 	print(ID, pick_names)
+ 	fit_value_l = [fit_value_l_list[i][pick] for i in range(len(labels))]
+ 	fit_value_m = [fit_value_m_list[i][pick] for i in range(len(labels))]
+ 	fit_value_h = [fit_value_h_list[i][pick] for i in range(len(labels))]
+ 	count_n = 8
+ 	chisq = [float(chisq[i]) for i in range(len(chisq))]
+ 	sort_chisq = np.argsort(np.asarray(chisq))    
+ 	Chisq_best = chisq[sort_chisq[0]]
+ 	#Chisq_last= chisq[sort_chisq[-1]]
+ 	Chisq_last= chisq[sort_chisq[count_n-1]]
+ 	weight = np.zeros(len(chisq))
+ 	inf_alp = (Chisq_last-Chisq_best) / (2*2.* Chisq_best)
+ 	for i in sort_chisq[:count_n]:
+ 	#for i in range(len(sort_chisq)):
+ 	#    weight[i] = np.exp(-1/2. * (chisq[i]-Chisq_best)/(Chisq_best))
+ 	    weight[i] = np.exp(-1/2. * (chisq[i]-Chisq_best)/(Chisq_best* inf_alp))
+ 	weighted_value = np.sum(np.array(fit_value_m)*weight) / np.sum(weight)
+ 	rms_value = np.sqrt(np.sum((np.array(fit_value_m)-weighted_value)**2*weight) / np.sum(weight))
+ 	weighted_value_list.append(weighted_value)
+ 	rms_value_list.append(rms_value)
+ 	# print pick_names[pick]
+ 	# print "weighted_value, rms_value:", round(weighted_value,2), round(rms_value,2)
 
 fig = plt.figure(figsize=(12, 10))
 x_pos = np.arange(len(IDs))
 for i in range(len(IDs)):
-	plt.errorbar(x_pos[i], weighted_value_list[i], yerr=rms_value_list[i], fmt = '*')#, color=color[ftype%2],
+ 	plt.errorbar(x_pos[i], weighted_value_list[i], yerr=rms_value_list[i], fmt = '*')#, color=color[ftype%2],
     # plt.legend(numpoints=1,ncol=2,loc=2,prop={'size':18})
 plt.title('The overall fitting results',fontsize=25)
 plt.ylabel(pick_names[pick],fontsize=25)
